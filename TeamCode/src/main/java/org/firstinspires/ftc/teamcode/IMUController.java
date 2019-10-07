f59d3b8923fb6e7bf285a25816e3a5226a99d083
Jordan <sockmonkeys4443@gmail.com>
2019-10-07 16:38:36 -0500

    Added the getAngle method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUController.java
@@ -4,11 +4,15 @@ import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 public class IMUController {
     public BNO055IMU imu;
-    public Orientation lastAngles = new Orientation();
+    public Orientation angles = new Orientation();
+    public float heading;
     BNO055IMU.Parameters parameters;
 
     public void init(HardwareMap hardwareMap, Telemetry telemetry) {
@@ -25,4 +29,16 @@ public class IMUController {
         telemetry.addData("Status", "calibrating imu...");
         telemetry.update();
     }
+
+    public float getAngle() {
+        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        heading = AngleUnit.DEGREES.normalize(AngleUnit.DEGREES.fromUnit(angles.angleUnit, angles.firstAngle));
+        return heading;
+    }
+
+    /*
+    public void resetAngle() {
+        lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        globalAngle = 0;
+    }*/
 }
