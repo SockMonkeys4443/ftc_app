b3ccc83b0ecb91f9ce9296ccb1cd719deba779fb
4443sockmonkeys <drostandrew@gmail.com>
2019-10-23 15:48:44 -0500

    Update DarkTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DarkTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DarkTeleOp.java
@@ -14,7 +14,9 @@ public class DarkTeleOp extends SuperDark {
 
 
     enum DriveMode {NORMAL, DIAGONAL, ADJUSTED}
+
     double drivePower;
+
     enum PositionMode {NORMAL, TESTONE, TESTTWO}
 
 
@@ -30,16 +32,13 @@ public class DarkTeleOp extends SuperDark {
     public void darkRunning() {
 
         //drive
-        if (driveMode==DriveMode.NORMAL) {
+        if (driveMode == DriveMode.NORMAL) {
             driveNormal(drivePower);
-        }
-        else if (driveMode==DriveMode.DIAGONAL) {
+        } else if (driveMode == DriveMode.DIAGONAL) {
             driveDiagonal(drivePower);
-        }
-        else if (driveMode==DriveMode.ADJUSTED) {
+        } else if (driveMode == DriveMode.ADJUSTED) {
             driveCorrected(drivePower);
-        }
-        else {
+        } else {
             driveNormal(drivePower);
         }
 
@@ -49,14 +48,14 @@ public class DarkTeleOp extends SuperDark {
             toggleDrive();
         }
 
-        if(bPressed()) {
+        if (bPressed()) {
             toggleServo();
         }
 
-        if(xPressed()) {
+        if (xPressed()) {
             togglePosition();
         }
-        if(aPressed()) {
+        if (aPressed()) {
             toggleSpeed();
         }
 
@@ -64,15 +63,20 @@ public class DarkTeleOp extends SuperDark {
         arm.armPower(gamepad2.right_stick_y);
 
         //telemetry
-        telemetry.addData("Heading",imuController.getAngle()+" "+imuController.getAngle360());
-        telemetry.addData("HeadingA:",imuController.getAngle360());
+        telemetry.addData("Heading", imuController.getAngle() + " " + imuController.getAngle360());
+        telemetry.addData("HeadingA:", imuController.getAngle360());
         telemetry.addData("driveMode", driveMode);
         telemetry.addData("Drive Power", drivePower);
         telemetry.update();
 
         //claw
-        if(gamepad2.left_trigger!=0) {arm.clawPower(gamepad2.left_trigger);} else if (gamepad2.right_trigger!=0) {
-            arm.clawPower(-gamepad2.right_trigger); } else {arm.clawPower(0);}
+        if (gamepad2.left_trigger != 0) {
+            arm.clawPower(gamepad2.left_trigger);
+        } else if (gamepad2.right_trigger != 0) {
+            arm.clawPower(-gamepad2.right_trigger);
+        } else {
+            arm.clawPower(0);
+        }
 
         //imu
         imuController.updatePosition();
@@ -80,97 +84,142 @@ public class DarkTeleOp extends SuperDark {
     }
 
 
+
+
+
     //button control
-    boolean aButtonState;
-    boolean bButtonState;
-    boolean xButtonState;
-    boolean yButtonState;
+    boolean aWasPressed;
+    boolean bWasPressed;
+    boolean xWasPressed;
+    boolean yWasPressed;
 
     boolean aPressed() {
-        if( gamepad1.a) {
-            aButtonState = !aButtonState;
-
+        if (!aWasPressed && gamepad1.a) {
+            aWasPressed = true;
+            return true;
+        } else {
+            return false;
         }
-        return aButtonState;
     }
+
     boolean bPressed() {
-        if( gamepad1.b) {
-            bButtonState = !bButtonState;
-
+        if (!bWasPressed && gamepad1.b) {
+            bWasPressed = true;
+            return true;
+        } else {
+            return false;
         }
-        return bButtonState;
     }
+
     boolean xPressed() {
-        if( gamepad1.x) {
-            xButtonState = !xButtonState;
-
+        if (!xWasPressed && gamepad1.x) {
+            xWasPressed = true;
+            return true;
+        } else {
+            return false;
         }
-        return aButtonState;
+
     }
+
     boolean yPressed() {
-        if( gamepad1.y) {
-            yButtonState = !yButtonState;
-
+        if (!yWasPressed && gamepad1.y) {
+            yWasPressed = true;
+            return true;
+        } else {
+            return false;
         }
-        return yButtonState;
     }
 
-
-    //toggles
-    void toggleDrive() {
-        if (driveMode==DriveMode.NORMAL) {driveMode=DriveMode.DIAGONAL;}
-        //else if (driveMode==DriveMode.DIAGONAL) {driveMode=DriveMode.ADJUSTED;}
-        else {driveMode=DriveMode.NORMAL;}
-    }
-    void toggleServo() {
-        if (foundServo.getPosition()==0) {foundServo.setPosition(1);}
-        else {foundServo.setPosition(0);}
-    }
-    void togglePosition() {
-        if (positionMode==PositionMode.NORMAL) {positionMode=PositionMode.TESTONE; imuController.stopTracking(); imuController.startTracking();}
-        else if (positionMode==PositionMode.TESTONE) {positionMode=PositionMode.TESTTWO; imuController.stopTracking(); imuController.startTracking2();}
-        else {positionMode=PositionMode.NORMAL; imuController.stopTracking();}
-    }
-    void toggleSpeed() {
-        if (drivePower==1) {drivePower=0.3;}
-        else {drivePower=1;}
-    }
-
-
-
-    //Drive Methods
-
-
-    void driveNormal(double power) {
-
-        double frontPower = -gamepad1.left_stick_y*power;
-        double sidePower = gamepad1.left_stick_x*power;
-
-        double turnPower = gamepad1.right_stick_x*power;
-
-        if (java.lang.Math.abs(sidePower) < java.lang.Math.abs(frontPower) / 3) {
-            sidePower = 0;
+    void buttonUpdate() {
+        if (aWasPressed && !gamepad1.a) {
+            aWasPressed = false;
+        }
+        if (bWasPressed && !gamepad1.b) {
+            bWasPressed = false;
+        }
+        if (xWasPressed && !gamepad1.x) {
+            xWasPressed = false;
+        }
+        if (yWasPressed && !gamepad1.y) {
+            yWasPressed = false;
         }
 
-        if (java.lang.Math.abs(frontPower) < java.lang.Math.abs(sidePower) / 3) {
-            frontPower = 0;
-        }
-
-        drive.frontLeft.setPower((frontPower +sidePower) +turnPower);
-        drive.frontRight.setPower((frontPower-sidePower) -turnPower);
-        drive.backLeft.setPower((frontPower-sidePower) +turnPower);
-        drive.backRight.setPower((frontPower+sidePower) -turnPower);
-
-        telemetry.addData("frontPower", frontPower);
-        telemetry.addData("sidePower", sidePower);
     }
 
 
-    void driveDiagonal(double power) {
-        double frontPower = -gamepad1.left_stick_y*power;
-        double sidePower = gamepad1.left_stick_x*power;
+        //toggles
+        void toggleDrive() {
+            if (driveMode == DriveMode.NORMAL) {
+                driveMode = DriveMode.DIAGONAL;
+            }
+            //else if (driveMode==DriveMode.DIAGONAL) {driveMode=DriveMode.ADJUSTED;}
+            else {
+                driveMode = DriveMode.NORMAL;
+            }
+        }
+        void toggleServo() {
+            if (foundServo.getPosition() == 0) {
+                foundServo.setPosition(1);
+            } else {
+                foundServo.setPosition(0);
+            }
+        }
+        void togglePosition() {
+            if (positionMode == PositionMode.NORMAL) {
+                positionMode = PositionMode.TESTONE;
+                imuController.stopTracking();
+                imuController.startTracking();
+            } else if (positionMode == PositionMode.TESTONE) {
+                positionMode = PositionMode.TESTTWO;
+                imuController.stopTracking();
+                imuController.startTracking2();
+            } else {
+                positionMode = PositionMode.NORMAL;
+                imuController.stopTracking();
+            }
+        }
+        void toggleSpeed() {
+            if (drivePower == 1) {
+                drivePower = 0.3;
+            } else {
+                drivePower = 1;
+            }
+        }
 
-        double turnPower = gamepad1.right_stick_x*power;
+
+        //Drive Methods
+
+
+        void driveNormal(double power){
+
+            double frontPower = -gamepad1.left_stick_y * power;
+            double sidePower = gamepad1.left_stick_x * power;
+
+            double turnPower = gamepad1.right_stick_x * power;
+
+            if (java.lang.Math.abs(sidePower) < java.lang.Math.abs(frontPower) / 3) {
+                sidePower = 0;
+            }
+
+            if (java.lang.Math.abs(frontPower) < java.lang.Math.abs(sidePower) / 3) {
+                frontPower = 0;
+            }
+
+            drive.frontLeft.setPower((frontPower + sidePower) + turnPower);
+            drive.frontRight.setPower((frontPower - sidePower) - turnPower);
+            drive.backLeft.setPower((frontPower - sidePower) + turnPower);
+            drive.backRight.setPower((frontPower + sidePower) - turnPower);
+
+            telemetry.addData("frontPower", frontPower);
+            telemetry.addData("sidePower", sidePower);
+        }
+
+
+        void driveDiagonal(double power){
+            double frontPower = -gamepad1.left_stick_y * power;
+            double sidePower = gamepad1.left_stick_x * power;
+
+            double turnPower = gamepad1.right_stick_x * power;
 
         /*
         drive.frontLeft.setPower((frontPower +sidePower) +turnPower);
@@ -178,88 +227,99 @@ public class DarkTeleOp extends SuperDark {
         drive.backLeft.setPower((frontPower-sidePower) +turnPower);
         drive.backRight.setPower((frontPower+sidePower) -turnPower);
         */
-        //frontLeft forward
-        //backRight forward
+            //frontLeft forward
+            //backRight forward
 
-        drive.frontLeft.setPower(frontPower+turnPower);
-        drive.backRight.setPower(frontPower-turnPower);
-        drive.frontRight.setPower(-sidePower-turnPower);
-        drive.backLeft.setPower(-sidePower+turnPower);
+            drive.frontLeft.setPower(frontPower + turnPower);
+            drive.backRight.setPower(frontPower - turnPower);
+            drive.frontRight.setPower(-sidePower - turnPower);
+            drive.backLeft.setPower(-sidePower + turnPower);
 
 
+        }
+
+        void driveWeird ( double power){
+
+            double frontPower = -gamepad1.left_stick_y;
+            double sidePower = gamepad1.left_stick_x;
+            double turnPower = gamepad1.right_stick_x;
+
+            if (frontPower > 0.5) {
+                frontPower = 1;
+            } else if (frontPower < -0.5) {
+                frontPower = -1;
+            } else {
+                frontPower = 0;
+            }
+            if (sidePower > 0.5) {
+                sidePower = 1;
+            } else if (sidePower < -0.5) {
+                sidePower = -1;
+            } else {
+                sidePower = 0;
+            }
+            //if(turnPower>0.5) {turnPower=1;} else if (turnPower<-0.5) {turnPower=-1;} else {turnPower=0;}
+
+
+            drive.frontLeft.setPower((frontPower + sidePower) + turnPower);
+            drive.frontRight.setPower((frontPower - sidePower) - turnPower);
+            drive.backLeft.setPower((frontPower - sidePower) + turnPower);
+            drive.backRight.setPower((frontPower + sidePower) - turnPower);
+
+        }
+
+        void driveCorrected ( double power){
+
+            //gets the robots current angle
+            float angle = imuController.getAngle360();
+
+            //values for FL and BR motors
+            float[] modFL = new float[]{
+                    -java.lang.Math.abs((angle - 315) / 45),
+                    java.lang.Math.abs((angle - 315) / 45),
+                    1,
+                    1,
+                    java.lang.Math.abs((angle - 135) / 45),
+                    -java.lang.Math.abs((angle - 135) / 45),
+                    -1,
+                    -1,
+                    -java.lang.Math.abs((angle - 315) / 45),
+                    java.lang.Math.abs((angle - 315) / 45)
+            };
+
+            //values for FR and BL motors
+            float[] modFR = new float[]{
+                    1,
+                    1,
+                    java.lang.Math.abs((angle - 45) / 45),
+                    -java.lang.Math.abs((angle - 45) / 45),
+                    -1,
+                    -1,
+                    -java.lang.Math.abs((angle - 225) / 45),
+                    java.lang.Math.abs((angle - 225) / 45),
+                    1,
+                    1
+            };
+
+            //finds which value to use from the arrays, uses max function to prevent an error
+            int angleTest = java.lang.Math.max((int) (java.lang.Math.floor(angle / 45)) + 2, 9);
+
+
+            double frontPower1 = -gamepad1.left_stick_y * power * modFL[angleTest];
+            double frontPower2 = -gamepad1.left_stick_y * power * modFR[angleTest];
+
+            double sidePower1 = gamepad1.left_stick_x * power * modFL[angleTest - 2];
+            double sidePower2 = gamepad1.left_stick_x * power * modFR[angleTest - 2];
+
+            double turnPower = gamepad1.right_stick_x * power;
+
+            drive.frontLeft.setPower((frontPower1 + sidePower1) + turnPower);
+            drive.frontRight.setPower((frontPower2 - sidePower2) - turnPower);
+            drive.backLeft.setPower((frontPower2 - sidePower2) + turnPower);
+            drive.backRight.setPower((frontPower1 + sidePower1) - turnPower);
+
+        }
 
 
     }
 
-    void driveWeird(double power) {
-
-        double frontPower = -gamepad1.left_stick_y;
-        double sidePower = gamepad1.left_stick_x;
-        double turnPower = gamepad1.right_stick_x;
-
-        if(frontPower>0.5) {frontPower=1;} else if (frontPower<-0.5) {frontPower=-1;} else {frontPower=0;}
-        if(sidePower>0.5) {sidePower=1;} else if (sidePower<-0.5) {sidePower=-1;} else {sidePower=0;}
-        //if(turnPower>0.5) {turnPower=1;} else if (turnPower<-0.5) {turnPower=-1;} else {turnPower=0;}
-
-
-        drive.frontLeft.setPower((frontPower +sidePower) +turnPower);
-        drive.frontRight.setPower((frontPower-sidePower) -turnPower);
-        drive.backLeft.setPower((frontPower-sidePower) +turnPower);
-        drive.backRight.setPower((frontPower+sidePower) -turnPower);
-
-    }
-
-    void driveCorrected(double power) {
-
-        //gets the robots current angle
-        float angle = imuController.getAngle360();
-
-        //values for FL and BR motors
-        float[] modFL = new float[] {
-                -java.lang.Math.abs((angle - 315) / 45),
-                java.lang.Math.abs((angle - 315) / 45),
-                1,
-                1,
-                java.lang.Math.abs((angle - 135) / 45),
-                -java.lang.Math.abs((angle - 135) / 45),
-                -1,
-                -1,
-                -java.lang.Math.abs((angle - 315) / 45),
-                java.lang.Math.abs((angle - 315) / 45)
-        };
-
-        //values for FR and BL motors
-        float[] modFR = new float[] {
-                1,
-                1,
-                java.lang.Math.abs((angle - 45) / 45),
-                -java.lang.Math.abs((angle - 45) / 45),
-                -1,
-                -1,
-                -java.lang.Math.abs((angle - 225) / 45),
-                java.lang.Math.abs((angle - 225) / 45),
-                1,
-                1
-        };
-
-        //finds which value to use from the arrays, uses max function to prevent an error
-        int angleTest = java.lang.Math.max((int) (java.lang.Math.floor(angle / 45)) + 2, 9);
-
-
-        double frontPower1 = -gamepad1.left_stick_y * power * modFL[angleTest];
-        double frontPower2 = -gamepad1.left_stick_y * power * modFR[angleTest];
-
-        double sidePower1 = gamepad1.left_stick_x * power * modFL[angleTest - 2];
-        double sidePower2 = gamepad1.left_stick_x * power * modFR[angleTest - 2];
-
-        double turnPower = gamepad1.right_stick_x * power;
-
-        drive.frontLeft.setPower((frontPower1 + sidePower1) + turnPower);
-        drive.frontRight.setPower((frontPower2 - sidePower2) - turnPower);
-        drive.backLeft.setPower((frontPower2 - sidePower2) + turnPower);
-        drive.backRight.setPower((frontPower1 + sidePower1) - turnPower);
-
-    }
-
-
-}
