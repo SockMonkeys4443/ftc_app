3584640ccfd139664c933988e5b6861f2560527b
4443sockmonkeys <drostandrew@gmail.com>
2019-11-08 16:20:23 -0600

    Update DarkAutoFoundationRed.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DarkAutoFoundationRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DarkAutoFoundationRed.java
@@ -13,40 +13,37 @@ public class DarkAutoFoundationRed extends SuperDark {
     @Override
     public void darkRunning() {
         timer.restart();
-        drive.goBackwards(0.5);
-        while (opModeIsActive() && timer.check() < 4 && distance.cmBack() > 40); //TODO: make a method to do this part automatically.
+        drive.goBackwards(0.8);
+        while (opModeIsActive() && timer.check() < 4 && distance.cmBack() > 50); //TODO: make a method to do this part automatically.
         drive.stopAll();
         drive.goBackwards(0.3);
         while (opModeIsActive() && timer.check() < 5 && distance.cmBack() > 10);
         drive.stopAll();
         foundServo.setPosition(1);
-        sleep(1500);
-        drive.strafeRight(0.35);
+        sleep(1000);
+        drive.strafeRight(0.70);
         timer.restart();
         //strafes to the right while the foundation is still in front of us
         //while (opModeIsActive() && timer.check() < 5 && distance.cmBack() < 25);
-        sleep(750);
+        sleep(325);
         //goes for a little bit of extra time
-        drive.goForwards(0.5);
-        sleep(1000);
+        drive.goForwards(1);
+        sleep(500);
         drive.stopAll();
 
-        //TODO: make the turn method stop when the opMode stops
-        //Ok, I think I got it to work... still needs testing - Jordan.
+
+        drive.turn(-90,1);
+
+
         foundServo.setPosition(0.17); //~30 degrees from the 0 point - that being the top
         sleep(300);
 
-        drive.turn(-90,0.5);
+        //something broke here
+        sleep(2500);
 
+        //drive.turn(90, 0.75);
+        drive.turnTo(0, 0.75);
 
-        sleep(500); //TODO: Andrew, this changed so idk if itll be weird still
-
-        drive.turn(-90, 0.5); //was turnto method h julgyjtghj
-        drive.turn(-10, 0.2);
-
-        //TODO: Andrew, this is the number one thing you need to change. the turn methods don't keep the robot
-        //TODO: very square as it is, so you need to fix them so the robot is square against the bridge
-        //TODO: right now, the robot is not close enough to the center bridge for another robot to park
 
         //deploy arm
         arm.armPower(0.5f);
@@ -64,20 +61,12 @@ public class DarkAutoFoundationRed extends SuperDark {
 
 
 
-        drive.strafeLeft(0.5); //TODO: change this to a faster speed.. temporary for testing
-        sleep(1250);
-
-        arm.armPower(0.5f);
+        //go "backwards" behind the bridge using timing
+        drive.goForwards(1);
         sleep(250);
 
-        drive.goBackwards(0.3);
-        sleep(300);
-        /*
-        drive.strafeLeft(0.3); //TODO: change this to a faster speed.. temporary for testing
-        timer.restart();
-        while(opModeIsActive() && timer.check() < 3 && distance.cmFront() > 30);
-        drive.stopAll();
-        */
+        drive.strafeRight(1);
+        sleep(500);
         stop();
     }
 }
