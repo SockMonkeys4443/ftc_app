67084323652a0c1e8397d2b6ef24db8ccda7ced5
4443sockmonkeys <drostandrew@gmail.com>
2019-11-13 15:57:42 -0600

    added observeColorValue
    added a method that looks at the color values for a little while and puts them into a list. it then returns the average of the values in the list

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DarkColorSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DarkColorSensor.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
+//importing arrayList class for reading a color's values
+import java.util.ArrayList;
+
 import com.qualcomm.robotcore.hardware.ColorSensor;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -30,5 +33,44 @@ public class DarkColorSensor {
         telemetry.addData("Red", colorSensor.red());
         telemetry.addData("Green", colorSensor.green());
         telemetry.addData("Blue", colorSensor.blue());
+    }
+
+    void observeColorValues(int mSeconds, Telemetry telemetry) {
+        ArrayList<Integer> redValues = new ArrayList<>();
+        ArrayList<Integer> greenValues = new ArrayList<>();
+        ArrayList<Integer> blueValues = new ArrayList<>();
+
+        for (int i = 0; i <= mSeconds / 10; i++) {
+            redValues.add(colorSensor.red());
+            greenValues.add(colorSensor.green());
+            blueValues.add(colorSensor.blue());
+
+            opMode.sleep(10);
+        }
+
+
+        int redTotal = 0;
+        for (int value: redValues) {
+            redTotal += value;
+        }
+        int redAverage = redTotal / redValues.size();
+        telemetry.addData("Red Average", redAverage);
+
+
+        int greenTotal = 0;
+        for (int value: greenValues) {
+            greenTotal += value;
+        }
+        int greenAverage = greenTotal / greenValues.size();
+        telemetry.addData("Green Average", greenAverage);
+
+
+        int blueTotal = 0;
+        for (int value: blueValues) {
+            blueTotal += value;
+        }
+        int blueAverage = blueTotal / blueValues.size();
+        telemetry.addData("Blue Average", blueAverage);
+
     }
 }
