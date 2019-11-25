e19c609a8132597b16ad74f41a99eed81b9f584f
4443sockmonkeys <drostandrew@gmail.com>
2019-11-25 16:04:36 -0600

    created blueSkystone

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueSkystoneAuto.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="TEST color sensor blue", group="working")
+public class BlueSkystoneAuto extends SuperDark {
+
+    @Override
+    public void darkInit() {
+        //
+    }
+
+    @Override
+    public void darkRunning() {
+
+        timer.restart();
+
+
+
+    }
+
+    
+}
