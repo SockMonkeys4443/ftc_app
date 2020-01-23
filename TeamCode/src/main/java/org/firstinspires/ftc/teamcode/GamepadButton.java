47bb2d31266f51a1f94d0277181597decb159d1a
4443sockmonkeys <drostandrew@gmail.com>
2020-01-23 15:53:38 -0600

    cleaned up gamepad code
    probably has issues

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadButton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadButton.java
@@ -2,8 +2,65 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.Gamepad;
 
-public class GamepadButton {
-    boolean pressed = false;
-    Gamepad gamepad;
+public class GamepadButton extends SuperDark {
 
+    Gamepad gamepad;
+    Boolean aWasPressed;
+    Boolean bWasPressed;
+    Boolean xWasPressed;
+    Boolean yWasPressed;
+
+    @Override
+    public void darkInit() {
+        //
+    }
+
+    @Override
+    public void darkRunning() {
+
+    }
+
+    public GamepadButton(Gamepad gamepad) {
+        this.gamepad = gamepad;
+    }
+
+    public void updateButtons() {
+        if(aWasPressed&&!gamepad.a){aWasPressed=false;}
+        if(bWasPressed&&!gamepad.b){bWasPressed=false;}
+        if(xWasPressed&&!gamepad.x){xWasPressed=false;}
+        if(yWasPressed&&!gamepad.y){yWasPressed=false;}
+    }
+
+    boolean aPressed() {
+        if (!aWasPressed && gamepad.a) {
+            aWasPressed = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    boolean bPressed() {
+        if (!bWasPressed && gamepad.b) {
+            bWasPressed = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    boolean xPressed() {
+        if (!xWasPressed && gamepad.x) {
+            xWasPressed = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    boolean yPressed() {
+        if (!yWasPressed && gamepad.y) {
+            yWasPressed = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
